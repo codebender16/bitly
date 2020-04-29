6ecccb2d61ae9eacba4c6942faefa3cbc5a140a1
CoderAcademy-MEL <angusl.zanshin@gmail.com>
2020-04-29 16:22:59 +1000

    created url input button

--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -1,5 +1,23 @@
 class UrlController < ApplicationController
+
   def index
     
   end
+
+  def show
+    
+  end
+
+  def create
+    @link = Link.create(params[:original_url])
+    p link
+  end
+
+
+  # private
+
+  # def link_params
+  #   params.require(:link).permit(:original_url)
+  # end
+
 end
